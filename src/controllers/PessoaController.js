46e9071f61744576167163d11e55f32ab8f2bcc7
CarlosEduardo7700 <carlos.msilva@quintoandar.com.br>
2025-02-08 16:40:14 -0300

    feat: Criando a Rota e o Controller de Pessoas

---
+++ src/controllers/PessoaController.js
@@ -0,0 +1,13 @@
+class PessoaController {
+
+  static async getAll(req, res) {
+    try {
+      // Asssar a Model
+    } catch (error) {
+      // Tratamento de erro
+    }
+  }
+
+}
+
+module.exports = PessoaController;
